a68c71b48471c83d22287896f5d66193ab637afb
dnlzz <adeglio@gmail.com>
2015-11-17 17:31:00 -0500

    indexJumboAded

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	include("header.php"); 
 ?>
 
-    <!-- Main jumbotron for a primary marketing message or call to action -->
+   
     <div class="jumbotron">
       <div class="container">
         <h1>Welcome!</h1>
@@ -13,22 +13,22 @@
     </div>
 
     <div class="container-fluid feature">
-      <!-- Example row of columns -->
+      
 	<div class="container">
       <div class="row">
-      <div class="col-md-2"></div>
-        <div class="col-md-4">
-          <h2>Feature: Adding Employees/Products</h2>
-          <p>This feature allows you to add an employee or product. Amazing!</p>
-          <p><a class="btn btn-default" href="#" role="button">Learn more &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-          <h2>Feature: Updating Employess/Products</h2>
-          <p>This feature allows you to update or remove an employee or product. Amazing!</p>
-          <p><a class="btn btn-default" href="#" role="button">Learn More &raquo;</a></p>
-       </div>
-       <div class="col-md-2"></div>
+	      <div class="col-md-2"></div>
+	        <div class="col-md-4">
+	          <h2>Feature: Adding Employees/Products</h2>
+	          <p>This feature allows you to add an employee or product. Amazing!</p>
+	          <p><a class="btn btn-default" href="#" role="button">Learn more &raquo;</a></p>
+	        </div>
+	        <div class="col-md-4">
+	          <h2>Feature: Updating Employess/Products</h2>
+	          <p>This feature allows you to update or remove an employee or product. Amazing!</p>
+	          <p><a class="btn btn-default" href="#" role="button">Learn More &raquo;</a></p>
+	       </div>
+	       <div class="col-md-2"></div>
+	      </div>
       </div>
-      </div>
-</div>
+	</div>
 <?php include("footer.php"); ?>
