aa1a8d490e46f2863fc8a5ff0614fa78f4a89db7
dnlzz <adeglio@gmail.com>
2015-11-18 20:07:42 -0500

    php500error

--- updateEmp.php
+++ updateEmp.php
@@ -5,7 +5,7 @@
 
 		// Create connection
 	$conn = mysql_connect($hostname, $username, $password);
-
+	mysql_select_db("Supermarket");
 	// Check connection
 	if (!$conn) {
 	    die("Connection failed: " . mysqli_connect_error());
@@ -18,26 +18,29 @@
 	SELECT * FROM EMPLOYEE
 	Have Edit/Delete Buttons
 
-	<?php $s = "SELECT * FROM `DEPARTMENT`"; 
+	<?php $s = "SELECT * FROM DEPARTMENT"; 
 
-		( $t = mysql_query ( $s ) ) or die ( mysql_error() ); //Sends sql query to database
-
+		( $t = mysql_query($s) ) or die ( mysql_error() ); //Sends sql query to database
+		    
 		echo "<table>";
 
-		while ( $r = mysql_fetch_array($t) )//getting data from sql table and displaying in html table
+		while ( $r = mysql_fetch_assoc($t) )//getting data from sql table and displaying in html table
 		{
 			echo "<tr>"
 		        echo   "<td>";
-			echo $r["Department_ID"]; //this is how you retrieve information from each row
+				echo $r["DEPARTMENT_ID"]; //this is how you retrieve information from each row
 		        echo   "</td>";
+
 		        echo   "<td>";
-			echo $r["Department_name"]; //this is how you retrieve information from each row
+				echo $r["DEPARTMENT_NAME"]; //this is how you retrieve information from each row
 		        echo   "</td>";
 		    echo   "</tr>";
 		}
 
 		echo "</table>";
+		
 	 ?>
+	
 
 	</div>
 </div>
