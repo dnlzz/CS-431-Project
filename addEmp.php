249c48227a43bff08fd835b3045dc6efd26435a3
Tony <adeglio@gmail.com>
2015-11-29 19:40:15 -0500

    add employee working - add produce up next

--- addEmp.php
+++ addEmp.php
@@ -19,8 +19,8 @@
 
 	<?php
 			// define variables and set to empty values
-			$firstNameErr = $lastNameErr = $phNumErr = $streetErr = $cityErr = $stateErr = $dobErr = $salaryErr = "";
-			$firstName = $lastName = $phNum = $street = $city = $state = $dob = $salary = "";
+			$firstNameErr = $lastNameErr = $phNumErr = $streetErr = $cityErr = $stateErr = $dobErr = $salaryErr = $departmentErr = "";
+			$firstName = $lastName = $phNum = $street = $city = $state = $dob = $salary = $department = "";
 			$success = "";
 
 			if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -95,13 +95,21 @@
 			    	$salary = test_input($_POST["salary"]);
 			    }
 
+
+			    if (empty($_POST["department"])) {
+			    	$departmentErr = "Department is required";
+			   	} else {
+			    	$department = test_input($_POST["department"]);
+		   		}
+
+
 			    if ( (!empty($_POST["firstName"])) and (!empty($_POST["lastName"])) and (!empty($_POST["phNum"])) and (!empty($_POST["address"]))
-			    		and (!empty($_POST["city"])) and (!empty($_POST["state"])) ) 
+			    		and (!empty($_POST["city"])) and (!empty($_POST["state"])) and (!empty($_POST["department"])) ) 
 			    {
-			    	$sql = "INSERT INTO Employee (Employee_first_name, Employee_last_name, Employee_salary, Employee_date_of_birth, Employee_phone, Employee_street_name, Employee_city, Employee_state) VALUES ('$firstName', '$lastName', '$salary', '$dob', '$phNum', '$street', '$city', '$state')";
-			    	if ( $result = mysql_query($sql) ) {
-			    		$success = "Employee added!";
-			    		echo $success;	
+			    	$sql = "INSERT INTO EMPLOYEE (Employee_first_name, Employee_last_name, Employee_salary, Employee_date_of_birth, Employee_phone, Employee_street_name, Employee_city, Employee_state, Department_ID) VALUES ('$firstName', '$lastName', '$salary', '$dob', '$phNum', '$street', '$city', '$state', 2);";
+			    	
+			    	if ( mysql_query($sql) ) {
+			    		$success = "Employee added!";	
 			    	} else {
 			    		echo "ERROR ADDING EMPLOYEE";
 		    		}
@@ -125,33 +133,33 @@
 
 		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 		  <div class="form-group">
-	    	<label for="firstName">First Name  <span class="error">* <?php echo $firstNameErr;?></span></label>
+	    	<label for="firstName">First Name  * <span class="error"><?php echo $firstNameErr;?></span></label>
 	    	<input type="text" class="form-control" name="firstName" placeholder="First Name">
 		  	
 		  </div>
 
 		  <div class="form-group">
-	    	<label for="lastName">Last Name  <span class="error">* <?php echo $lastNameErr;?></span></label>
+	    	<label for="lastName">Last Name  * <span class="error"><?php echo $lastNameErr;?></span></label>
 	    	<input type="text" class="form-control" name="lastName" placeholder="Last Name">
 		  </div>
 
   		  <div class="form-group">
-	    	<label for="phNum">Phone Number  <span class="error">* <?php echo $phNumErr;?></span></label>
+	    	<label for="phNum">Phone Number  * <span class="error"><?php echo $phNumErr;?></span></label>
 	    	<input type="text" class="form-control" name="phNum" placeholder="ex 555-555-5555">
 		  </div>
 
   		  <div class="form-group">
-	    	<label for="address">Street Address  <span class="error">* <?php echo $streetErr;?></span></label>
+	    	<label for="address">Street Address  * <span class="error"><?php echo $streetErr;?></span></label>
 	    	<input type="text" class="form-control" name="address" placeholder="ex 123 Main St">
 		  </div>
 
 		  <div class="form-group">
-	    	<label for="city">City  <span class="error">* <?php echo $cityErr;?></span></label>
+	    	<label for="city">City  * <span class="error"><?php echo $cityErr;?></span></label>
 	    	<input type="text" class="form-control" name="city" placeholder="City">
 		  </div>
 
 		  <div class="form-group">
-	    	<label for="state">State  <span class="error">* <?php echo $stateErr;?></span></label>
+	    	<label for="state">State  * <span class="error"><?php echo $stateErr;?></span></label>
 	    	<input type="text" class="form-control" name="state" placeholder="State">
 		  </div>
 
@@ -165,7 +173,21 @@
 	    	<input type="text" class="form-control" name="salary" placeholder="ex 36000">
 		  </div>
 
-		
+		  <div class="form-group">
+	    	<label for="department">Department  <span class="error">* <?php echo $departmentErr;?></span></label>
+				<select name="department">
+					<option value="">Select...</option>
+				    <option value="1">Produce</option>
+				    <option value="2">Meat</option>
+				    <option value="3">Seafood</option>
+				    <option value="4">Bakery</option>
+				    <option value="5">Deli</option>
+				    <option value="6">Pharmacy</option>
+				    <option value="7">Specialty</option>
+				    <option value="8">Frozen</option>
+			  	</select>		
+		  </div>
+	
 
 		  <button type="submit" class="btn btn-success" name="submit">Add Employee</button>
 		</form>
