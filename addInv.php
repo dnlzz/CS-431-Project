19cd58f887b83d910456778831e8c70775583bca
Tony <adeglio@gmail.com>
2015-12-03 17:52:57 -0500

    inventory add item not working

--- addInv.php
+++ addInv.php
@@ -17,10 +17,26 @@
 
 ?>
 
+<?php 
+
+	if (isset($_POST['addSubmit'])) {
+		$prodID = $_POST['product'];
+		$wareID = $_POST['wID'];
+		$quantity = $_POST['qty'];
+
+		$add_sql = "INSERT INTO INVENTORY (Product_ID, Warehouse_ID, Number_of_item_in_stock)
+					VALUES ('$prodID', '$wareID', '$quantity');";
+
+		$res = ( mysql_query($add_sql) ) or die(mysql_error());
+
+		if ($res) { header("Location: index.php"); }
+
+
+	}
+?>
 
 <div class="container-fluid feature">
 	<div class="content">
-
 	<h1>Add Inventory Items</h1>
 	<br>
 	<div class="form-group">
@@ -54,17 +70,52 @@
 		$location = $arr[1];
 ?>
 	
-		<h3>Add Product to <?php echo $location; ?> Warehouse</h3>
+	<h3>Add Product to <?php echo $location; ?> Warehouse</h3>
 
-	Product: Dropdown
-	Quantity: text
+	<?php 
 
-	Submit
+		$sql = "SELECT * FROM PRODUCT";
+		$result = ( mysql_query($sql) ) or die( mysql_error() );
+
+	 ?>
+
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+	<input type="hidden" name="wID" value="<?php echo $wID; ?>">
+
+	<div class="form-group">
+		<select class="form-control" name="product">
+		<option value="0" selected="selected">Select Product...</option>
+	
+	<?php 	
+
+	while ($row = mysql_fetch_assoc($result)) {
+		$pID = $row['Product_ID'];
+		$pName = $row['Product_name'];
+	?>
+		
+    	<option value="<?php echo $pID; ?>"><?php echo $pName; ?></option>
+	
+	<?php } ?>
+
+  		</select>
+
+	<div class="form-group">
+		<label for="qty">Quantity</label>
+	    <input type="text" class="form-control" name="qty" placeholder="Quantity">
+	</div>
+
+    <button type="addSubmit" class="btn btn-success">Stock it!</button>
+
+	</form>
+
 
 <?php } ?>
 
+
 	</div>
 </div>
+
 
 
 
